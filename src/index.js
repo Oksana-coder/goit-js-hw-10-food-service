f94658ce73e18efb4047d66a504cccbd2863a6a3
Oksana Palamarchuk <8444186@gmail.com>
2021-04-11 20:08:34 +0200

    Updated files

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { onPageReboot, themeSwitchCheckboxRef, onThemeChange } from './js/theme-switch';
+import menuItemTpl from './templates/menu-item.hbs';
+import menuItems from './menu.json'
+
+
+onPageReboot();
+
+themeSwitchCheckboxRef.addEventListener('change', onThemeChange);
+
+
+const menuContainer = document.querySelector('.js-menu');
+const menuItemsMarkup = createMenuItemsMarkup(menuItems);
+
+menuContainer.insertAdjacentHTML("beforeend", menuItemsMarkup)
+
+function createMenuItemsMarkup(menuItems) {
+    return menuItems.map(menuItemTpl).join('');
+}
